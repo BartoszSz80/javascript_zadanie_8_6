a69dd4e042df6122cea5832041adc9d6a5ca2e47
BartoszSz80 <bartek@bartweb.eu>
2017-04-03 10:34:48 +0200

    files added

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+// plik scripts.js
+var triangle1Area = getTriangleArea(10, 15);
+var triangle2Area = getTriangleArea(2, 5); 
+var triangle3Area = getTriangleArea(4, 9); 
+
+	function getTriangleArea(a, h) {
+		if (a <= 0)
+			console.log('Nieprawidłowe dane');
+		else if (h <= 0)
+			console.log('Nieprawidłowe dane');
+
+	return (a*h/2);
+	}
+	console.log( getTriangleArea(10,6) )
+	console.log(triangle1Area); 
+	console.log(triangle2Area); 
+	console.log(triangle3Area); 
